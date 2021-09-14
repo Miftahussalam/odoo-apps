f42d599506103f6e5ea5f6784d03c681c604f6bc
Miftahussalam <miftah_salam@ymail.com>
2021-09-15 06:56:30 +0700

    ms_timesheet_start_stop: calculate duration from start and end_time

--- ms_timesheet_start_stop/models/account_analytic_line.py
+++ ms_timesheet_start_stop/models/account_analytic_line.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
-# class ms_timesheet_start_stop(models.Model):
-#     _name = 'ms_timesheet_start_stop.ms_timesheet_start_stop'
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    @api.depends('start_time', 'end_time', 'break_unit_amount')
+    @api.multi
+    def _get_unit_amount(self):
+        for rec in self :
+            unit_amount = 0
+            if rec.start_time and rec.end_time :
+                unit_amount = rec.end_time - rec.start_time
+                unit_amount = unit_amount.total_seconds() / 60 / 60
+                if rec.break_unit_amount :
+                    unit_amount -= rec.break_unit_amount
+            rec.unit_amount = unit_amount
+
+    start_time = fields.Datetime(string='Start Time')
+    end_time = fields.Datetime(string='End Time')
+    break_unit_amount = fields.Float(string='Break Time (Hour(s))')
+    unit_amount = fields.Float(compute='_get_unit_amount', store=True)
