e5af9f18b2ee1f71056ba030188dc29c5040e734
Miftahussalam <miftah_salam@ymail.com>
2022-08-28 19:33:04 +0700

    berhasil kirim data wishes

--- sync_data/controllers/controllers.py
+++ sync_data/controllers/controllers.py
@@ -8,7 +8,6 @@ class WeddingWishes(http.Controller):
     @http.route('/wishes', type='http', auth="none", methods=['GET'], csrf=False)
     def create(self, **data):
         wedding_wishlist_obj = request.env['wedding.wishes'].sudo()
-        print('\n data', data)
         vals = {
             'name': data.get('name', False),
             'image_filename': data.get('image_filename', '').split('\\')[-1],
