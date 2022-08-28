f0f845c43c3ee9f099131ac799bdadf84b3f7f3f
Miftahussalam <miftah_salam@ymail.com>
2022-08-28 16:50:25 +0700

    berhasil kirim wishes data

--- sync_data/controllers/controllers.py
+++ sync_data/controllers/controllers.py
@@ -5,8 +5,18 @@ from odoo.http import request
 
 class WeddingWishes(http.Controller):
 
-    @http.route('/wishes', type='json', auth="none", methods=['POST'], csrf=False)
-    def create(self, **post):
+    @http.route('/wishes', type='http', auth="none", methods=['GET'], csrf=False)
+    def create(self, **data):
         wedding_wishlist_obj = request.env['wedding.wishes'].sudo()
-        print('\n post', post)
-        return post
+        print('\n data', data)
+        wedding_wishlist_obj.create({
+            'name': data.get('name', False),
+            'image': data.get('image', False),
+            'image_filename': data.get('image_filename', False),
+            'description': data.get('description', False),
+            'wishes': data.get('wishes', False),
+            'invitation_code': data.get('invitation_code', False),
+            'invitation_name': data.get('invitation_name', False),
+            'invitation_description': data.get('invitation_description', False),
+        })
+        return 'successfully'
