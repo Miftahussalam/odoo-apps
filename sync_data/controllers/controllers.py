a03191ca208e3bb51e2fdb5a327b0210790cb10b
Miftahussalam <miftah_salam@ymail.com>
2022-08-28 19:24:53 +0700

    berhasil kirim data wishes

--- sync_data/controllers/controllers.py
+++ sync_data/controllers/controllers.py
@@ -9,14 +9,18 @@ class WeddingWishes(http.Controller):
     def create(self, **data):
         wedding_wishlist_obj = request.env['wedding.wishes'].sudo()
         print('\n data', data)
-        wedding_wishlist_obj.create({
+        vals = {
             'name': data.get('name', False),
-            'image': data.get('image', False),
-            'image_filename': data.get('image_filename', False),
+            'image_filename': data.get('image_filename', '').split('\\')[-1],
             'description': data.get('description', False),
             'wishes': data.get('wishes', False),
             'invitation_code': data.get('invitation_code', False),
             'invitation_name': data.get('invitation_name', False),
             'invitation_description': data.get('invitation_description', False),
-        })
+        }
+        if data.get('image', 'undefined') not in ['undefined']:
+            if type(data['image']) == str and 'base64' in data['image']:
+                image_vals = data['image'].split('base64,')
+                vals['image'] = image_vals[-1]
+        wedding_wishlist_obj.create(vals)
         return 'successfully'
