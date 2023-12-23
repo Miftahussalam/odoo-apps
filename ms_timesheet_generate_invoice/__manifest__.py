ccfad0d0dd476e9ba0872113689318c6ea066acd
Miftahussalam <miftah_salam@ymail.com>
2023-12-23 17:08:34 +0700

    copy to v16 (belum upgrade)

---
+++ ms_timesheet_generate_invoice/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Timesheet Generate Invoice",
+
+    'summary': """
+        Generate customer invoice based on timesheet
+    """,
+
+    'description': """
+
+    """,
+
+    'author': "Miftahussalam",
+    'website': "https://blog.miftahussalam.com/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Timesheet',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+        'account',
+        'analytic',
+        'hr_timesheet',
+    ],
+
+    # always loaded
+    'data': [
+        # 'views/account_analytic_line_views.xml',
+        # 'views/project_project_views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+
+    ],
+}
