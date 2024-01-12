06c35a364d7fb0e52b29271589d46b28eca48321
Miftahussalam <miftah_salam@ymail.com>
2024-01-12 15:50:00 +0700

    ms_timesheet_partner: upgrade to v17

---
+++ ms_timesheet_partner/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Timesheet Customer",
+    'summary': """
+        Customer can login to system and check timesheet for them
+    """,
+    'description': """
+
+    """,
+    'author': "Miftahussalam",
+    'website': "https://blog.miftahussalam.com/",
+    'category': 'Timesheet',
+    'version': '17.0.1.0.0',
+    'depends': [
+        'base',
+        'account',
+        'product',
+        'hr_timesheet',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/res_groups.xml',
+        'security/ir_rule.xml',
+        'views/account_analytic_line_views.xml',
+    ],
+    'demo': [
+
+    ],
+    'images': [
+        'static/description/images/main_screenshot.png',
+    ],
+    'license': 'LGPL-3',
+}
