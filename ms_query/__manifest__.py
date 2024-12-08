db5b93762e024eaa7f93eba28f0cd2831f540202
Miftahussalam <miftah_salam@ymail.com>
2024-12-08 17:05:59 +0700

    downgrade

--- ms_query/__manifest__.py
+++ ms_query/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "Miftahussalam",
     "website": "https://blog.miftahussalam.com/",
     "category": "Extra Tools",
-    "version": "17.0.1.0.0",
+    "version": "16.0.1.0.0",
     "depends": [
         "base",
         "mail",
--- ms_report_stock/__manifest__.py
+++ ms_report_stock/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Miftahussalam",
     "website": "https://blog.miftahussalam.com/",
     "category": "Inventory/Inventory",
-    "version": "17.0.1.0.0",
+    "version": "16.0.1.0.0",
     "depends": [
         "base",
         "stock",
