847e1f7924dea62db81d5406cc37a78fd88c682d
Miftahussalam <miftah_salam@ymail.com>
2024-12-08 15:37:37 +0700

    initial upload and description

---
+++ ms_magic_button/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    "name": "Magic Button",
+    "summary": """
+        Run actions in one click button
+    """,
+    "description": """
+    """,
+    "author": "Miftahussalam",
+    "website": "https://blog.miftahussalam.com/",
+    "category": "Extra Tools",
+    "version": "17.0.1.0.0",
+    "depends": [
+        "base",
+        "mail",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/ms_magic_button.xml",
+    ],
+    "demo": [
+
+    ],
+    "images": [
+        "static/description/images/main_screenshot.png",
+    ],
+    "license": "LGPL-3",
+}
---
+++ ms_magic_button/wizard/__init__.py
@@ -0,0 +1,3 @@
+from . import (
+    ms_magic_button
+)
